a5a7d9f242b75f79dffe7453b0b1af264531a888
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-10-22 15:36:19 -0400

    Add step 3.

--- app/Main.hs
+++ app/Main.hs
@@ -78,6 +78,71 @@ userAPI = Proxy
 app2 :: Application
 app2 = serve userAPI server2
 
+type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
+      :<|> "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
+      :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
+
+data Position = Position
+  { xCoord :: Int
+  , yCoord :: Int
+  } deriving Generic
+
+instance ToJSON Position
+
+newtype HelloMessage = HelloMessage { msg :: String }
+  deriving Generic
+
+instance ToJSON HelloMessage
+
+data ClientInfo = ClientInfo
+  { clientName :: String
+  , clientEmail :: String
+  , clientAge :: Int
+  , clientInterestedIn :: [String]
+  } deriving Generic
+
+instance FromJSON ClientInfo
+instance ToJSON ClientInfo
+
+data Email = Email
+  { from :: String
+  , to :: String
+  , subject :: String
+  , body :: String
+  } deriving Generic
+
+instance ToJSON Email
+
+emailForClient :: ClientInfo -> Email
+emailForClient c = Email from' to' subject' body'
+
+  where from'    = "great@company.com"
+        to'      = clientEmail c
+        subject' = "Hey " ++ clientName c ++ ", we miss you!"
+        body'    = "Hi " ++ clientName c ++ ",\n\n"
+                ++ "Since you've recently turned " ++ show (clientAge c)
+                ++ ", have you checked out our latest "
+                ++ intercalate ", " (clientInterestedIn c)
+                ++ " products? Give us a visit!"
+
+server3 :: Server API
+server3 = position
+    :<|> hello
+    :<|> marketing
+    where position :: Int -> Int -> Handler Position
+          position x y = return (Position x y)
+
+          hello :: Maybe String -> Handler HelloMessage
+          hello mname = return . HelloMessage $ case mname of
+              Nothing -> "Hello, anonymous coward"
+              Just n  -> "Hello, " ++ n
+          marketing :: ClientInfo -> Handler Email
+          marketing clientInfo = return (emailForClient clientInfo)
+
+
+
+userAPI3 :: Proxy API
+userAPI3 = Proxy
 
 main :: IO ()
-main = run 8081 app2
+main = run 8081 (serve userAPI3 server3)
