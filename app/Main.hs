5a7ee740b52433d73e9e739d9c8fb0696dc55cba
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-10-22 13:46:43 -0400

    Second step.

--- app/Main.hs
+++ app/Main.hs
@@ -49,16 +49,35 @@ users1 =
     , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
     ]
 
+isaac, albert :: User
+isaac  = User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683 3  1)
+albert = User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
+
+users2 :: [User]
+users2 = [isaac, albert]
+
 type UserAPI1 = "users" :> Get '[JSON] [User]
+
+type UserAPI2
+    =    "users"  :> Get '[JSON] [User]
+    :<|> "albert" :> Get '[JSON]  User
+    :<|> "isaac"  :> Get '[JSON]  User
 
 server1 :: Server UserAPI1
 server1 = return users1
 
-userAPI :: Proxy UserAPI1
+server2 :: Server UserAPI2
+server2
+    =    return users2
+    :<|> return albert
+    :<|> return isaac
+
+userAPI :: Proxy UserAPI2
 userAPI = Proxy
 
-app1 :: Application
-app1 = serve userAPI server1
+app2 :: Application
+app2 = serve userAPI server2
+
 
 main :: IO ()
-main = run 8081 app1
+main = run 8081 app2
