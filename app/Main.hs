d9906baea2f6fed0dea76db47424fd741a02b004
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-10-28 15:14:55 -0400

    Serve git objects.

--- app/Main.hs
+++ app/Main.hs
@@ -21,6 +21,8 @@ import Data.ByteString (ByteString)
 import Data.List
 import Data.Maybe
 import Data.String.Conversions
+import Data.Text
+import Data.Text.Encoding
 import Data.Time.Calendar
 import GHC.Generics
 import Lucid
@@ -34,115 +36,23 @@ import Text.Blaze.Html.Renderer.Utf8
 import qualified Data.Aeson.Parser
 import qualified Text.Blaze.Html
 
-data User = User
-   { name              :: String
-   , age               :: Int
-   , email             :: String
-   , registration_date :: Day
-   } deriving (Eq, Show, Generic)
+import Duffer.Unified
+import Duffer.Loose.Objects (GitObject)
+import Duffer.Loose.JSON
+import Duffer.WithRepo
 
-instance ToJSON User
+type API = "git" :> Capture "ref" Text :> Get '[JSON] GitObject
 
-users1 :: [User]
-users1 =
-    [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683 3  1)
-    , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
-    ]
+server :: Server API
+server = serveObject
 
-isaac, albert :: User
-isaac  = User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683 3  1)
-albert = User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
+serveObject :: Text -> Handler GitObject
+serveObject textRef = do
+    let ref = encodeUtf8 textRef
+    liftIO $ fromJust <$> withRepo ".git" (readObject ref)
 
-users2 :: [User]
-users2 = [isaac, albert]
-
-type UserAPI1 = "users" :> Get '[JSON] [User]
-
-type UserAPI2
-    =    "users"  :> Get '[JSON] [User]
-    :<|> "albert" :> Get '[JSON]  User
-    :<|> "isaac"  :> Get '[JSON]  User
-
-server1 :: Server UserAPI1
-server1 = return users1
-
-server2 :: Server UserAPI2
-server2
-    =    return users2
-    :<|> return albert
-    :<|> return isaac
-
-userAPI :: Proxy UserAPI2
+userAPI :: Proxy API
 userAPI = Proxy
 
-app2 :: Application
-app2 = serve userAPI server2
-
-type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
-      :<|> "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
-      :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
-
-data Position = Position
-  { xCoord :: Int
-  , yCoord :: Int
-  } deriving Generic
-
-instance ToJSON Position
-
-newtype HelloMessage = HelloMessage { msg :: String }
-  deriving Generic
-
-instance ToJSON HelloMessage
-
-data ClientInfo = ClientInfo
-  { clientName :: String
-  , clientEmail :: String
-  , clientAge :: Int
-  , clientInterestedIn :: [String]
-  } deriving Generic
-
-instance FromJSON ClientInfo
-instance ToJSON ClientInfo
-
-data Email = Email
-  { from :: String
-  , to :: String
-  , subject :: String
-  , body :: String
-  } deriving Generic
-
-instance ToJSON Email
-
-emailForClient :: ClientInfo -> Email
-emailForClient c = Email from' to' subject' body'
-
-  where from'    = "great@company.com"
-        to'      = clientEmail c
-        subject' = "Hey " ++ clientName c ++ ", we miss you!"
-        body'    = "Hi " ++ clientName c ++ ",\n\n"
-                ++ "Since you've recently turned " ++ show (clientAge c)
-                ++ ", have you checked out our latest "
-                ++ intercalate ", " (clientInterestedIn c)
-                ++ " products? Give us a visit!"
-
-server3 :: Server API
-server3 = position
-    :<|> hello
-    :<|> marketing
-    where position :: Int -> Int -> Handler Position
-          position x y = return (Position x y)
-
-          hello :: Maybe String -> Handler HelloMessage
-          hello mname = return . HelloMessage $ case mname of
-              Nothing -> "Hello, anonymous coward"
-              Just n  -> "Hello, " ++ n
-          marketing :: ClientInfo -> Handler Email
-          marketing clientInfo = return (emailForClient clientInfo)
-
-
-
-userAPI3 :: Proxy API
-userAPI3 = Proxy
-
 main :: IO ()
-main = run 8081 (serve userAPI3 server3)
+main = run 8081 (serve userAPI server)
