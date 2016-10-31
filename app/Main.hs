78135e9a0dcb521f816f69e65b6a75060ebfd3bd
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-10-31 15:24:45 -0400

    Remove unnecessary imports.

--- app/Main.hs
+++ app/Main.hs
@@ -10,22 +10,8 @@ module Main where
 import Prelude ()
 import Prelude.Compat
 
-import Control.Monad.Except
-import Control.Monad.Reader
-import Data.Aeson.Compat
-import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import Data.ByteString (ByteString)
-import Data.List
-import Data.Maybe
-import Data.String.Conversions
 import Data.Text
 import Data.Text.Encoding
-import Data.Time.Calendar
-import GHC.Generics
-import Lucid
-import Network.HTTP.Media ((//), (/:))
-import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 import System.Directory
