8906e203ededc901ba6b7f3a88060e4b596f4a52
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-03-02 10:11:07 -0500

    Simplify Server.

--- src/Server.hs
+++ src/Server.hs
@@ -1,10 +1,5 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Server where
@@ -16,23 +11,10 @@ import Control.Monad.Except
 import Control.Monad.Reader
 import Data.Aeson.Compat
 import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import Data.ByteString (ByteString)
-import Data.List
-import Data.Maybe
-import Data.String.Conversions
-import Data.Time.Calendar
 import GHC.Generics
-import Lucid
-import Network.HTTP.Media ((//), (/:))
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
-import System.Directory
-import Text.Blaze
-import Text.Blaze.Html.Renderer.Utf8
-import qualified Data.Aeson.Parser
-import qualified Text.Blaze.Html
 
 data User = User
    { name              :: String
@@ -59,6 +41,3 @@ userAPI = Proxy
 
 app1 :: Application
 app1 = serve userAPI server1
-
-main :: IO ()
-main = run 8081 app1
