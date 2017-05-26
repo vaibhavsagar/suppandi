2dd166138a3be788be2d2cb884b9e37e75d14917
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-05-26 08:08:22 +0800

    Use provided RefJSON

--- src/Server.hs
+++ src/Server.hs
@@ -12,24 +12,14 @@ import Data.Proxy           (Proxy(..))
 import Data.Text            (Text, intercalate, unpack)
 import Data.Text.Encoding   (encodeUtf8, decodeUtf8)
 import Duffer.Unified       (readObject, resolveRef, writeObject)
-import Duffer.Loose.Objects (Ref)
 import Duffer.WithRepo      (WithRepo, liftIO, withRepo)
-import Duffer.JSON          (GitObjectJSON(GitObjectJSON))
+import Duffer.JSON          (GitObjectJSON(GitObjectJSON), RefJSON(RefJSON))
 import Servant
-
-newtype JSONRef = JSONRef Ref
-
-instance ToJSON JSONRef where
-    toJSON     (JSONRef ref) = object ["ref" .= decodeUtf8 ref]
-    toEncoding (JSONRef ref) = pairs  ("ref" .= decodeUtf8 ref)
-
-instance FromJSON JSONRef where
-    parseJSON (Object v) = coerce <$> (encodeUtf8 <$> v .: "ref")
 
 type API = Capture "path" FilePath :>
     (    "git" :>
         (    Capture "ref"   Text          :> Get  '[JSON] GitObjectJSON
-        :<|> ReqBody '[JSON] GitObjectJSON :> Post '[JSON] JSONRef
+        :<|> ReqBody '[JSON] GitObjectJSON :> Post '[JSON] RefJSON
         )
     :<|> "ref" :> CaptureAll "path" Text :> Get '[JSON] GitObjectJSON
     )
@@ -56,5 +46,5 @@ serveRef repoPath textPath = execute repoPath (resolveRef path) >>=
         (return . coerce)
     where path = unpack $ intercalate "/" textPath
 
-writeObj :: FilePath -> GitObjectJSON -> Handler JSONRef
-writeObj path obj = JSONRef <$> execute path (writeObject (coerce obj))
+writeObj :: FilePath -> GitObjectJSON -> Handler RefJSON
+writeObj path = fmap coerce . execute path . writeObject . coerce
