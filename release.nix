395a9dde73c5251d1bdf613b539bbdca84aca27b
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-07-17 15:20:36 +0800

    Move dependencies to release.nix

--- release.nix
+++ release.nix
@@ -1,17 +1,18 @@
 let
-  config = {
-    packageOverrides = pkgs: rec {
-      haskellPackages = pkgs.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: rec {
-          suppandi =
-            haskellPackagesNew.callPackage ./default.nix { };
-        };
-      };
-    };
+  pkgs = import <nixpkgs> {};
+  duffer-repo = pkgs.fetchFromGitHub {
+    owner = "vaibhavsagar";
+    repo = "duffer";
+    rev = "818a2ef2624c193f398795bd4bcb63af8214b3c1";
+    sha256 = "0lgrh5gyqfipj2ryikr2i9xb8vyazn79i2qcidyxkx064bkxrirx";
   };
+  duffer = pkgs.haskell.lib.dontCheck (
+    pkgs.haskellPackages.callPackage ( "${duffer-repo}/duffer" ) {}
+  );
+  duffer-json = pkgs.haskell.lib.dontCheck (
+    pkgs.haskellPackages.callPackage ( "${duffer-repo}/duffer-json" ) { inherit duffer; }
+  );
 
-  pkgs = import <nixpkgs> { inherit config; };
-
-in
-  { suppandi = pkgs.haskellPackages.suppandi;
-  }
+in {
+  suppandi = pkgs.haskellPackages.callCabal2nix "suppandi" ./. { inherit duffer duffer-json; };
+}
